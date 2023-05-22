88d012c7659cc6a3de10a9f7be6e2908ee946f43
skullKid <novatinas2@gmail.com>
2023-05-22 17:01:50 -0400

    Nueva Version

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent {
   
   ngOnInit() {
     this.metaService.addTag( { name:'Novatinas.cl',content:"Article Description"});
-    this.metaService.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' });
+    
   }
   
   
